04ce614d8d51a026d71eee3dc04bcb291262421c
Tô Ngọc Khánh <khanhuitse05@gmail.com>
2017-08-09 14:56:59 +0700

    +Add project

---
+++ Localization/Assets/Localizations/Samples/LoadingLocalization.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingLocalization : MonoBehaviour {
+
+    public Dropdown dropLanguage;
+    public GameObject guiMain;
+    public GameObject setting;
+    // Use this for initialization
+    void Start() {
+        // Language
+        LocalizationData.LoadLocalization();
+        dropLanguage.onValueChanged.AddListener(delegate { OnLanguageDropdown(); });
+
+        string[] _knownLanguages = Localization.knownLanguages;
+        List<string> _Languages = ConverArray<string>(_knownLanguages);
+        dropLanguage.ClearOptions();
+        dropLanguage.AddOptions(_Languages);
+        SetTextDropDown(dropLanguage, Localization.language, false);
+
+        isShow = false;
+        OnShowSetting();
+    }
+
+    bool isShow = false;
+    public void OnShowSetting()
+    {
+        isShow = !isShow;
+        guiMain.SetActive(isShow);
+        setting.SetActive(!isShow);
+    }
+    public void OnLanguageDropdown()
+    {
+        Localization.language = dropLanguage.captionText.text;
+    }
+    List<T> ConverArray<T>(T[] paramArray)
+    {
+        if (paramArray == null)
+            return null;
+        List<T> list = new List<T>();
+        for (int i = 0; i < paramArray.Length; i++)
+        {
+            list.Add(paramArray[i]);
+        }
+        return list;
+    }
+    bool SetTextDropDown(Dropdown drop, string value, bool addnew = true)
+    {
+        for (int i = 0; i < drop.options.Count; i++)
+        {
+            if (drop.options[i].text == value)
+            {
+                drop.value = i;
+                return true;
+            }
+        }
+        if (addnew)
+        {
+            drop.options.Add(new Dropdown.OptionData { text = value });
+            drop.value = drop.options.Count - 1;
+        }
+        return false;
+    }
+}
---
+++ Localization/Assets/Localizations/Scripts/LanguageSelection.cs
@@ -0,0 +1,44 @@
+﻿/// <summary>
+/// Turns the popup list it's attached to into a language selection list.
+/// </summary>
+using UnityEngine;
+
+public class LanguageSelection : MonoBehaviour
+{
+    public GameObject pfItem;
+    public RectTransform content;
+    void Start()
+    {
+        GameObject _objItem;
+        LanguageItem _item;
+        int lenght = Localization.knownLanguages.Length;
+        for (int i = 0; i < lenght; i++)
+        {
+            _objItem = GameObject.Instantiate(pfItem) as GameObject;
+            _objItem.transform.SetParent(content);
+            _objItem.transform.localPosition = Vector3.zero;
+            _objItem.SetActive(true);
+
+            _item = _objItem.GetComponent<LanguageItem>();
+            _item.Init(Localization.knownLanguages[i], OnChange);
+            if (Localization.language == Localization.knownLanguages[i])
+            {
+                _item.OnChoose();
+            }
+        }
+        content.sizeDelta = new Vector2(content.sizeDelta.x, lenght * pfItem.GetComponent<RectTransform>().sizeDelta.y);
+    }
+    public void onShow()
+    {
+        gameObject.SetActive(true);
+    }
+    public void onHide()
+    {
+        gameObject.SetActive(false);
+    }
+    public void OnChange(string _value)
+    {
+        Localization.language = LanguageItem.current.id;
+        onHide();
+    }
+}
